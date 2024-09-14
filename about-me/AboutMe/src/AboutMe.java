6bddee55328d9ff578355faf18bc1cca3365806d
Maysa <maysasousaalves@gmail.com>
2024-09-13 20:43:42 -0400

    Applying the scanner class

--- about-me/AboutMe/src/AboutMe.java
+++ about-me/AboutMe/src/AboutMe.java
@@ -1,5 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
+        // Creating a Scanner object
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.CANADA);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.next();
+
+        System.out.println("Enter your lastname: ");
+        String lastName = scanner.next();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Enter your height: ");
+        double height = scanner.nextDouble();
+
+        System.out.println("Hello, my name is " + name + " " + lastName);
+        System.out.println("I'm " + age + " years old ");
+        System.out.println("My height is " + height + "cm ");
+
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+/*     public static void main(String[] args) {
 
         String name =  args [0];
         String lastName = args[1];
@@ -10,4 +47,5 @@ public class AboutMe {
         System.out.println("I'm " + age + " years old ");
         System.out.println("My height is " + height + "cm ");
     } 
+*/
 }
