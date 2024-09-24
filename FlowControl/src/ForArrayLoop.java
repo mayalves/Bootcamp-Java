20f595568971e7bc566333437e74840ff369b618
Maysa <maysasousaalves@gmail.com>
2024-09-23 20:57:34 -0400

    For each example

--- FlowControl/src/ForArrayLoop.java
+++ FlowControl/src/ForArrayLoop.java
@@ -3,8 +3,15 @@ public class ForArrayLoop {
   public static void main(String[] args) {
     String students[] = {"Felipe", "Maria", "Julia", "Marcos"};
 
+    for (String student : students){
+      System.out.println("Student name is: " + student);    
+    }
+
+    
+/* 
     for (int x = 0; x < students.length; x++) {
       System.out.println("The student at index x = " + x + " is " + students[x]);
     }
+*/
   }
 }
