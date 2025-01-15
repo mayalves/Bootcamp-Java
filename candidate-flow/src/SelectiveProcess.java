d011e8fe81271a46dd4633f97e1dfd9d6a9b197a
Maysa <maysasousaalves@gmail.com>
2025-01-15 17:25:52 -0500

    Selecting candidate

--- candidate-flow/src/SelectiveProcess.java
+++ candidate-flow/src/SelectiveProcess.java
@@ -1,9 +1,36 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class SelectiveProcess {
     public static void main(String[] args) {
-        analyzeCandidate(1900.0);
+     /* analyzeCandidate(1900.0);
         analyzeCandidate(2200.0);
-        analyzeCandidate(2000.0);
+        analyzeCandidate(2000.0); */
     }
+
+    static void candidateSelection(){
+        // Canditate array
+        String [] candidate = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int selectedCandidate = 0;
+        int currentCandidate = 0;
+        double baseSalary = 2000.0;
+
+        while (selectedCandidate < 5 && currentCandidate < candidate.length) {
+            String candte = candidate[currentCandidate];
+            double desiredSalary = desiredValue();
+
+            System.out.println("The candidate " + candte + " requested this salary amount: " + desiredSalary);
+            if (baseSalary >= desiredSalary) {
+                System.out.println("The candidate " + candidate + " was selected for the vacancy");
+                selectedCandidate++;
+            }
+            currentCandidate++;
+        }
+    }
+
+    static double desiredValue() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2000);
+    }
+
     static void analyzeCandidate(double desiredSalary){
         double baseSalary  = 2000.0;
         if (baseSalary > desiredSalary) {
